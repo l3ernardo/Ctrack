a132f54fd5fba4642e78875d9a37c6f3fc65129e
l3ernardo <l3ernardo@gmail.com>
2021-03-20 08:06:33 +0000

    cratrack remove typo

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,18 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class Home extends Component {
   render() {
     return (
-      <div className="container">
-        <h4>This is an React app that fetch´s data using context API, list group user´s and use google API key for user lacation</h4>
-        <h4>CSS is pure testing, creating stuff out the box, the idea is not use boostrap or material ui etc... this is for practice.</h4>
+      <div className='container'>
+        <h4>
+          This is a React app that fetch´s data using context API, list group
+          user´s and use google API key for user lacation
+        </h4>
+        <h4>
+          CSS is pure testing, creating stuff out the box, the idea is not use
+          boostrap or material ui etc... this is for practice.
+        </h4>
       </div>
-    )
+    );
   }
 }
